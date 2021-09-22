d22a04b8f6eace406b07e2f297f466e0893ce66b
lucimarsb <lucimarsan@outlook.com>
2021-09-22 20:02:17 -0300

    extraindo/refatorando para uma camada de producer

--- src/main/java/br/com/lucimarsb/ecommerce/NewOrderMain.java
+++ src/main/java/br/com/lucimarsb/ecommerce/NewOrderMain.java
@@ -4,6 +4,7 @@ import org.apache.kafka.clients.producer.*;
 import org.apache.kafka.common.protocol.types.Field;
 import org.apache.kafka.common.serialization.StringSerializer;
 
+import java.io.IOException;
 import java.util.Map;
 import java.util.Properties;
 import java.util.UUID;
@@ -12,31 +13,16 @@ import java.util.concurrent.ExecutionException;
 public class NewOrderMain {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
 
-        var producer =  new KafkaProducer<String, String>(properties());
-        for (var i = 0; i< 10; i++) {
-            var key = UUID.randomUUID().toString();
-            var value = key + ",67523,145";
-            var record = new ProducerRecord<String, String>("ECOMMERCE_NEW_ORDER", key, value);
-            Callback callback = (data, ex) -> {
-                if (ex != null) {
-                    ex.printStackTrace();
-                    ;
-                    return;
-                }
-                System.out.println("sucesso enviado " + data.topic() + ":::partition " + data.partition() + "/ offset " + data.offset() + "/ timestamp " + data.timestamp());
-            };
-            var email = "Obrigado pelo seu pedido! Já Estamos processando seu pedido!";
-            var emailRecord = new ProducerRecord<>("ECOMMERCE_SEND_EMAIL", key, email);
-            producer.send(record, callback).get();
-            producer.send(emailRecord, callback).get();
-        }
-    }
+        try (var dispatcher = new KafkaDispatcher()) {
+            for (var i = 0; i < 10; i++) {
 
-    private static Properties properties() {
-        var properties = new Properties();
-        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
-        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        return properties;
+                var key = UUID.randomUUID().toString();
+                var value = key + ",67523,145";
+                dispatcher.send("ECOMMERCE_NEW_ORDER", key, value);
+
+                var email = "Obrigado pelo seu pedido! Já Estamos processando seu pedido!";
+                dispatcher.send("ECOMMERCE_SEND_EMAIL", key, value);
+            }
+        }
     }
 }
