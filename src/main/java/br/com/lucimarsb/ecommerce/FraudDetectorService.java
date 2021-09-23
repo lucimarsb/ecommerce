3cf84ed194e533e5d9c5e40c2d2abb6ebb88d2c9
lucimarsb <lucimarsan@outlook.com>
2021-09-22 22:18:39 -0300

    Criando um serializador vustomizado do Kafka Verificar o conteúdo exato de uma mensagem Deserialização customizada customização por serviço Utilizando o GSON

--- src/main/java/br/com/lucimarsb/ecommerce/FraudDetectorService.java
+++ src/main/java/br/com/lucimarsb/ecommerce/FraudDetectorService.java
@@ -3,19 +3,24 @@ package br.com.lucimarsb.ecommerce;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
 
 public class FraudDetectorService {
 
     public static void main(String[] args) {
         var fraudService = new FraudDetectorService();
-        try (var service = new KafkaService(FraudDetectorService.class.getSimpleName(),
-                "ECOMMERCE_NEW_ORDER", fraudService::parse)) {
+        try (var service = new KafkaService<>(FraudDetectorService.class.getSimpleName(),
+                "ECOMMERCE_NEW_ORDER",
+                fraudService::parse,
+                Order.class,
+                Map.of())) {
             service.run();
         }
     }
 
-    private void parse(ConsumerRecord<String, String> record) {
+    private void parse(ConsumerRecord<String, Order> record) {
         System.out.println("________________________________________");
         System.out.println("Processing new order, checking for fraud");
         System.out.println(record.key());
--- src/main/java/br/com/lucimarsb/ecommerce/NewOrderMain.java
+++ src/main/java/br/com/lucimarsb/ecommerce/NewOrderMain.java
@@ -5,6 +5,7 @@ import org.apache.kafka.common.protocol.types.Field;
 import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.io.IOException;
+import java.math.BigDecimal;
 import java.util.Map;
 import java.util.Properties;
 import java.util.UUID;
@@ -13,15 +14,19 @@ import java.util.concurrent.ExecutionException;
 public class NewOrderMain {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
 
-        try (var dispatcher = new KafkaDispatcher()) {
-            for (var i = 0; i < 10; i++) {
+        try (var orderDispatcher = new KafkaDispatcher<Order>()) {
+            try (var emailDispatcher = new KafkaDispatcher<String>()) {
+                for (var i = 0; i < 10; i++) {
 
-                var key = UUID.randomUUID().toString();
-                var value = key + ",67523,145";
-                dispatcher.send("ECOMMERCE_NEW_ORDER", key, value);
+                    var userd = UUID.randomUUID().toString();
+                    var orderId = UUID.randomUUID().toString();
+                    var amount = new BigDecimal(Math.random() * 500 + 1);
+                    var order = new Order(userd, orderId, amount);
+                    orderDispatcher.send("ECOMMERCE_NEW_ORDER", userd, order);
 
-                var email = "Obrigado pelo seu pedido! Já Estamos processando seu pedido!";
-                dispatcher.send("ECOMMERCE_SEND_EMAIL", key, value);
+                    var email = "Obrigado pelo seu pedido! Já Estamos processando seu pedido!";
+                    emailDispatcher.send("ECOMMERCE_SEND_EMAIL", userd, email);
+                }
             }
         }
     }
