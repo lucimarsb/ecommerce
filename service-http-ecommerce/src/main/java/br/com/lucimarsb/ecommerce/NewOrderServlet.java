0b072a88faea3f89db89cd249576627467a64b9a
lucimar.santos <lucimar.santos@cabal.com.br>
2021-10-10 18:57:01 -0300

    usando servidor http embarcado usando um serviço http enviando uma nova ordem(Order) a partir do servidor Http utilizando as vantagens de um fast delegate

---
+++ service-http-ecommerce/src/main/java/br/com/lucimarsb/ecommerce/NewOrderServlet.java
@@ -0,0 +1,52 @@
+package br.com.lucimarsb.ecommerce;
+
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.util.UUID;
+import java.util.concurrent.ExecutionException;
+
+public class NewOrderServlet extends HttpServlet {
+
+    private final KafkaDispatcher<Order> orderDispatcher = new KafkaDispatcher<Order>();
+    private final KafkaDispatcher<String> emailDispatcher = new KafkaDispatcher<String>();
+
+    @Override
+    public void destroy() {
+        super.destroy();
+        orderDispatcher.close();
+        emailDispatcher.close();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try {
+
+            //bem simples, não tem questões de segurança ainda
+            var email = req.getParameter("email");
+            var amount = new BigDecimal(req.getParameter("amount"));
+
+            var orderId = UUID.randomUUID().toString();
+
+            var order = new Order(orderId, amount, email);
+            orderDispatcher.send("ECOMMERCE_NEW_ORDER", email, order);
+
+            var emailCode = "Obrigado pelo seu pedido! Já Estamos processando!";
+            emailDispatcher.send("ECOMMERCE_SEND_EMAIL", email, emailCode);
+
+            System.out.println("Nova order enviada com sucesso.");
+            resp.setStatus(HttpServletResponse.SC_OK);
+            resp.getWriter().println("Nova order enviada");
+
+        } catch (ExecutionException e) {
+            throw new ServletException(e);
+        } catch (InterruptedException e) {
+            throw new ServletException(e);
+        }
+    }
+}
