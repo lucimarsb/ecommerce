f1bac70daaeb652477c6aefd9de9c623ea55399a
lucimar.santos <lucimar.santos@cabal.com.br>
2021-10-12 02:33:49 -0300

    estudando como lidar com latest e earliest

--- service-email-new-order/src/main/java/br/com/lucimarsb/ecommerce/EmailNewOrderService.java
+++ service-email-new-order/src/main/java/br/com/lucimarsb/ecommerce/EmailNewOrderService.java
@@ -1,27 +1,22 @@
 package br.com.lucimarsb.ecommerce;
-import br.com.lucimarsb.ecommerce.consumer.KafkaService;
+
+import br.com.lucimarsb.ecommerce.consumer.ConsumerService;
+import br.com.lucimarsb.ecommerce.consumer.ServiceRunner;
 import br.com.lucimarsb.ecommerce.dispatcher.KafkaDispatcher;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 
-import java.math.BigDecimal;
-import java.util.Map;
 import java.util.concurrent.ExecutionException;
 
-public class EmailNewOrderService {
+public class EmailNewOrderService implements ConsumerService<Order> {
 
-    public static void main(String[] args) throws ExecutionException, InterruptedException {
-        var emailService = new EmailNewOrderService();
-        try (var service = new KafkaService<>(EmailNewOrderService.class.getSimpleName(),
-                "ECOMMERCE_NEW_ORDER",
-                emailService::parse,
-                Map.of())) {
-            service.run();
-        }
+    public static void main(String[] args) {
+
+        new ServiceRunner(EmailNewOrderService::new).start(1);
     }
 
-    private  final KafkaDispatcher<String> emailDispatcher = new KafkaDispatcher<>();
+    private final KafkaDispatcher<String> emailDispatcher = new KafkaDispatcher<>();
 
-    private void parse(ConsumerRecord<String, Message<Order>> record) throws ExecutionException, InterruptedException {
+    public void parse(ConsumerRecord<String, Message<Order>> record) throws ExecutionException, InterruptedException {
         System.out.println("________________________________________");
         System.out.println("Processando nova Order, preparando e-mail");
         System.out.println(record.value());
@@ -30,6 +25,16 @@ public class EmailNewOrderService {
         var order = message.getPayload();
         var id = message.getId().continueWith(EmailNewOrderService.class.getSimpleName());
         var emailCode = "Obrigado pelo seu pedido! Já Estamos processando!";
-        emailDispatcher.send("ECOMMERCE_SEND_EMAIL", order.getEmail(),  id, emailCode);
+        emailDispatcher.send("ECOMMERCE_SEND_EMAIL", order.getEmail(), id, emailCode);
+    }
+
+    @Override
+    public String getTopic() {
+        return "ECOMMERCE_NEW_ORDER";
+    }
+
+    @Override
+    public String getConsumerGroup() {
+        return EmailNewOrderService.class.getSimpleName();
     }
 }
